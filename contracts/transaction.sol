6181c762f976dee49806d320ca2e1893062ee52a
LizbetArias <lizbet.arias@vallegrande.edu.pe>
2025-03-26 19:59:51 -0500

    ContratoInteligente-Solidity

--- contracts/transaction.sol
+++ contracts/transaction.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <=0.9.0;
-contract Transaction{
+pragma solidity ^0.8.14;
 
- function setTransaction(address payable _recipient, uint256 _amount)public payable {
-      (_recipient).transfer(_amount);
- } 
+contract EthSender {
+    uint256 eth = 0.1 ether;
+
+    // Eventos
+    event sendStatus (bool success);
+    event callStatus (bool success, bytes data);
+
+    constructor () payable {}
+    receive () external payable {}
+
+    // Transferencia de ETH
+    function transferEthers(address payable _to) public payable {
+        require(msg.value >= eth);
+        _to.transfer(eth);
+    }
+
+    function getBalanceSender() public view returns (uint) {
+        return address(this).balance;
+    }
 }
